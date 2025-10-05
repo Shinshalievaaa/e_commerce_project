9d3bf65b8079009887e5a7af2ba727de5b988779
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-10-05 17:30:36 +0500

    update category.py

--- src/category.py
+++ src/category.py
@@ -1,14 +1,33 @@
+from src.product import Product
+
+
 class Category():
     """Класс категория для электронного магазина"""
     name: str
     description: str
-    products: list
+    __products: list
     number_of_categories = 0
     number_of_products = 0
 
     def __init__(self, name, description, products=None):
         self.name = name
         self.description = description
-        self.products = products if products is not None else []
+        self.__products = []
+            # products) if products is not None else []
+        for product in products:
+            self.__products.append(product)
+            Category.number_of_products += 1
         Category.number_of_categories += 1
-        Category.number_of_products = len(self.products)
+
+
+    def add_product(self, Product):
+        self.__products.append(Product)
+        Category.number_of_products += 1
+
+    @property
+    def products(self):
+        products_str = ""
+        for product in self.__products:
+            products_str += f'{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n'
+
+        return products_str
