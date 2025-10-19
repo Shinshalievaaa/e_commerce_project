89e76eecf4299fd4ad450e0766656c25e9ad2615
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-10-19 14:05:58 +0500

    added method __add__

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -3,8 +3,17 @@ from src.product import Product
 
 class LawnGrass(Product):
     """Подкласс Смартфон"""
+    __price: float
+
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         super().__init__(name, description, price, quantity)
+        self.__price = price
         self.country = country
         self.germination_period = germination_period
         self.color = color
+
+    def __add__(self, other):
+        if type(other) == LawnGrass:
+            return self.quantity * self.__price + other.quantity * other.__price
+        else:
+            raise TypeError()
--- src/smartphone.py
+++ src/smartphone.py
@@ -3,9 +3,17 @@ from src.product import Product
 
 class Smartphone(Product):
     """Подкласс Смартфон"""
+    __price: float
     def __init__(self, name, description, price, quantity, efficiency, model, memory, color):
         super().__init__(name, description, price, quantity)
+        self.__price = price
         self.efficiency = efficiency
         self.model = model
         self.memory = memory
         self.color = color
+
+    def __add__(self, other):
+        if type(other) == Smartphone:
+            return self.quantity * self.__price + other.quantity * other.__price
+        else:
+            raise TypeError()
