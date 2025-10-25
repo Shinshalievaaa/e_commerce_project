9ceb4bdf1679aa553f54c7e96994071301fae706
Shinshalievaaa <aigul_shinshaliyeva@mail.ru>
2025-10-25 19:44:56 +0500

    added new file test_utils.py and update test report

--- src/utils.py
+++ src/utils.py
@@ -28,7 +28,7 @@ def create_objects_from_json(data) -> list:
 
 
 if __name__ == "__main__":
-    raw_data = read_json("src/products.json")
+    raw_data = read_json("../data/products.json")
     categories = create_objects_from_json(raw_data)
     print(categories[0].name)
     print(categories[0].description)
